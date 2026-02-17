eb0a3027fac36076fb31cf5d43a727a725314cfc
windjimmy-hash <wind.jimmy@gmail.com>
2026-02-17 14:40:37 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -4,15 +4,3 @@ export default {
     autoprefixer: {},
   },
 }
-```
-
-4. Klik "Commit changes"
-
-**En maak ook een .gitignore aan:**
-1. Klik op "Add file" → "Create new file"
-2. Naam: `.gitignore`
-3. Plak deze inhoud:
-```
-node_modules
-dist
-.DS_Store
